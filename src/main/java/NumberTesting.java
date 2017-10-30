540d1d8e87facca32d99d043f55671d34d4fcd1b
armaothao <armaothao@hotmail.com>
2017-10-29 19:06:49 -0700

    - Added NumberTesting and fixed pom.xml for java compiler.

---
+++ src/main/java/NumberTesting.java
@@ -0,0 +1,16 @@
+public class NumberTesting {
+    public static void main(String args[]) {
+        Integer apple = 100;
+        String appleStr = apple.toString();
+        boolean types = false;
+
+        System.out.println(apple);
+
+        System.out.println(appleStr);
+
+        System.out.println(apple instanceof Integer);
+        System.out.println(appleStr instanceof String);
+
+
+    }
+}
